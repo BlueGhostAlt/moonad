0d3589683bf2411e3792ee80e9ce65c198384ada
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-06-19 20:34:59 +0300

    chore: added a rollup configuration file

---
+++ rollup.config.ts
@@ -0,0 +1,64 @@
+import { resolve } from "path"
+
+import ts from "@wessberg/rollup-plugin-ts"
+
+import commonjs from "@rollup/plugin-commonjs"
+import nodeResolve from "@rollup/plugin-node-resolve"
+
+import filesize from "rollup-plugin-filesize"
+import { terser } from "rollup-plugin-terser"
+
+const packageRoot = process.cwd()
+const dev = Boolean(process.env.ROLLUP_WATCH)
+
+const inputFile = resolve(packageRoot, "src/index.ts")
+
+const _package = require(resolve(packageRoot, "package.json"))
+const packageName = String(_package.name)
+
+const commonPlugins = [nodeResolve(), commonjs()]
+
+export default [
+    {
+        input: inputFile,
+        output: [
+            {
+                file: _package.main,
+                format: "cjs",
+                sourcemap: true
+            },
+            {
+                file: _package.browser,
+                sourcemap: true,
+                format: "umd",
+                name: packageName[0].toUpperCase() + packageName.substr(1)
+            }
+        ],
+        plugins: [...commonPlugins, ts(), !dev && terser()]
+    },
+    {
+        input: inputFile,
+        output: [
+            {
+                file: _package.module,
+                format: "esm",
+                sourcemap: true
+            }
+        ],
+        plugins: [
+            ...commonPlugins,
+            ts({
+                tsconfig: {
+                    declaration: true,
+                    ...require(resolve(__dirname, "tsconfig.json"))[
+                        "compilerOptions"
+                    ]
+                }
+            }),
+            !dev && terser(),
+            filesize({
+                showBrotliSize: true
+            })
+        ]
+    }
+]
