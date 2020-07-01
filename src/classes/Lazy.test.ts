b046ec017dd44050006dba4195b4aada88c809d9
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-07-01 10:43:37 +0300

    test: added a test for binding lazy values

--- src/classes/Lazy.test.ts
+++ src/classes/Lazy.test.ts
@@ -30,4 +30,16 @@ describe("Lazy class", () => {
         expect(greedyVal.map(mapper).value).to.deep.equal(mapper(value))
         expect(lazyVal.map(mapper).value).to.deep.equal(mapper(value))
     })
+
+    it("it binds a lazy value", () => {
+        const value = Math.random()
+
+        const greedyVal = Lazy.pure(value)
+        const lazyVal = Lazy.lazy(() => value)
+
+        const binder = (x: number) => Lazy.lazy(() => x * 2)
+
+        expect(greedyVal.bind(binder).value).to.deep.equal(binder(value).value)
+        expect(lazyVal.bind(binder).value).to.deep.equal(binder(value).value)
+    })
 })
