5c1ba34690f2d197f8d6965f65eb268609103cb2
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-07-27 04:07:46 +0300

    feat: added a fromarray method for transforming arrays into lazylists

--- src/classes/LazyList.ts
+++ src/classes/LazyList.ts
@@ -16,6 +16,23 @@ export class LazyList<T> {
         this._value = value
     }
 
+    static fromArray<T>(xs: T[]): LazyList<T> {
+        const toElem = (xs: T[]): LazyListNode<T> => {
+            const [y, ...ys] = xs
+
+            return Lazy.lazy(() => {
+                if (!xs.length) return null
+
+                return {
+                    head: Lazy.pure(y),
+                    tail: toElem(ys)
+                }
+            })
+        }
+
+        return new this(toElem(xs))
+    }
+
     public get head(): Lazy<T> | null {
         if (!this._value.value) return null
 
